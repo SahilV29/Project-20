f42df5458304b2489b5c2c086801dbf7c6bc9196
SahilV29 <72529855+SahilV29@users.noreply.github.com>
2020-11-09 18:53:47 -0600

    Add files via upload

--- p5.play-boilerplate-master (1)/p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master (1)/p5.play-boilerplate-master/sketch.js
@@ -2,18 +2,19 @@ var car,wall;
 var speed, weight;
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1000,600);
+
 
 speed= random(55,90);
 weight=random(400,1500);
-car=createSprite(50, 200, 50, 50)
-wall= createSprite(1500,200,60, height/2)
+car=createSprite(25, 200, 50, 50)
+wall= createSprite(800,200,60, height/2)
+car.velocityX=speed;
 }
 
 function draw() {
   background(255,255,255);  
-car.velocityX=speed;
+
 
 if(wall.x-car.x<(car.width+wall.width)/2){
 
